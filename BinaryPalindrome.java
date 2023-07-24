0e89746329b9d96c16637e4ed98d5f1f28a9b47e
Dhruv <Dhruvpandey928@gmail.com>
2023-07-24 10:47:19 +0530

    Updated

---
+++ BinaryPalindrome.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class BinaryPalindrome
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        String s = Integer.toBinaryString(n);
+        String rev="";
+        int len = s.length();
+        sc.close();
+        
+        for(int i=len-1;i>=0;i--)
+        {
+            rev = rev+  String.valueOf(s.charAt(i));
+        }
+        if(rev.equals(s))
+        {
+            System.out.println("yes");
+        }
+        else
+        {
+            System.out.println("no");
+        }
+    }
+}
